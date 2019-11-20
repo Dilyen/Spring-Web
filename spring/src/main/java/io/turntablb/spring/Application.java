e45ab1a4e7ace3af0900638ab4d81c988050cf3e
Dilyen <davidandohacquah@gmail.com>
2019-11-20 16:08:42 +0000

    added many functionalities

--- spring/src/main/java/io/turntablb/spring/Application.java
+++ spring/src/main/java/io/turntablb/spring/Application.java
@@ -1,7 +1,9 @@
 package io.turntablb.spring;
 
+import Controller.Maths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +12,8 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public Maths mathUtil(){
+		return new Maths();
+	}
 }
--- spring/src/test/java/io/turntablb/spring/ApplicationTests.java
+++ spring/src/test/java/io/turntablb/spring/ApplicationTests.java
@@ -1,13 +1,61 @@
 package io.turntablb.spring;
 
+import Controller.Maths;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
+	@Test
+	void testAdd() {
+		Maths maths = new Maths();
+		Integer actual = maths.add(3,9);
+		Integer expected = 4;
+		assertEquals(expected,actual);
+	}
+
+	private void assertEquals(Integer expected, Integer actual) {
+	}
 
 	@Test
-	void contextLoads() {
+	void testSubtract() {
+		Maths maths = new Maths();
+		Integer actual = maths.sub(6,6);
+		Integer expected = 1;
+		assertEquals(expected,actual);
 	}
+	@Test
+	void addNegativePositiveNumbers() {
+		Maths maths = new Maths();
+		Integer actual = maths.add(-10, 9);
+		Integer expected = 3;
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void subtractPositiveNumbers() {
+		Maths maths = new Maths();
+		Integer actual = maths.sub(10, 9);
+		Integer expected = 6;
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void subtractNegativeNumbers() {
+		Maths maths = new Maths();
+		Integer actual = maths.sub(-10, -9);
+		Integer expected = -6;
+		assertEquals(expected, actual);
+	}
+	@Test
+	void subtractNegativePositiveNumbers() {
+		Maths maths = new Maths();
+		Integer actual = maths.sub(-10, 9);
+		Integer expected = -7;
+		assertEquals(expected, actual);
+	}
+	/*@Test
+	void contextLoads() {
+	}*/
 
 }
